835b143666dd94b414d2bf0c8e815aac9d133b5d
Evan_Park <seya3927@gmail.com>
2022-09-07 01:19:37 +1000

    로그인 UI 그리기

--- app/src/main/java/com/example/uikotlinpractice_evan/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice_evan/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.uikotlinpractice_evan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if confirmBtn.setOnClickListener {
+            putiD = admin@test.com
+            putPW = 123456
+        }
 
+        Toast.makeText("관리자 입니다.", Toast.LENGTH_SHORT).show()
+
+        else
+
+        Toast.makeText("로그인 실패", Toast.LENGTH_SHORT).show()
 
     }
 }
